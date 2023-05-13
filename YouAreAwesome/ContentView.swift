5b7353178029f7aeb613c8a8cf1603c13092ec4b
Jeremy Taylor <jtaylor@jctaylor.org>
2023-05-13 13:26:52 -0500

    Code as of video Ch. 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var messageString = ""
+  @State private var messageString = "When the Genius Bar needs help, they call you!"
   
   var body: some View {
     VStack {
       Text(messageString)
         .font(.largeTitle)
         .fontWeight(.heavy)
+        .minimumScaleFactor(0.5)
+        .multilineTextAlignment(.center)
         .foregroundColor(.red)
+        .frame(height: 150)
+        .frame(maxWidth: .infinity)
+        .border(.orange, width: 1)
         .padding()
       
       HStack {
@@ -30,6 +35,7 @@ struct ContentView: View {
         }
         .buttonStyle(.borderedProminent)
       }
+      .border(.purple, width: 5)
       
       
     }
