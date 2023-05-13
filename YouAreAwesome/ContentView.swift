bd4ccea7a3620424cba2a2875ca01df6f203df15
Jeremy Taylor <jtaylor@jctaylor.org>
2023-05-13 15:58:38 -0500

    Code as of video Ch. 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,34 +11,43 @@ struct ContentView: View {
   @State private var messageString = "When the Genius Bar needs help, they call you!"
   
   var body: some View {
-    VStack {
-      Text(messageString)
-        .font(.largeTitle)
-        .fontWeight(.heavy)
-        .minimumScaleFactor(0.5)
-        .multilineTextAlignment(.center)
-        .foregroundColor(.red)
-        .frame(height: 150)
-        .frame(maxWidth: .infinity)
-        .border(.orange, width: 1)
-        .padding()
-      
-      HStack {
-        Button("Awesome") {
-          // Performed when button is pressed
-          messageString = "You Are Awesome!"
-        }
-        .buttonStyle(.borderedProminent)
+      VStack {
         
-        Button("Great") {
-          messageString = "You Are Great!"
+        Spacer()
+        
+        Text(messageString)
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .minimumScaleFactor(0.5)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.red)
+          .frame(height: 150)
+          .frame(maxWidth: .infinity)
+  //        .border(.orange, width: 1)
+          .padding()
+        
+        Spacer()
+        
+        
+        HStack {
+          Button("Awesome") {
+            // Performed when button is pressed
+            messageString = "You Are Awesome!"
+          }
+          .buttonStyle(.borderedProminent)
+          
+          Spacer()
+          
+          Button("Great") {
+            messageString = "You Are Great!"
+          }
+          .buttonStyle(.borderedProminent)
         }
-        .buttonStyle(.borderedProminent)
+  //      .border(.purple, width: 5)
+        .padding()
+        
+        
       }
-      .border(.purple, width: 5)
-      
-      
-    }
   }
 }
 
