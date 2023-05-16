8f9355a3493e66cdb4018466e9e043daea4a2b8a
Jeremy Taylor <jtaylor@jctaylor.org>
2023-05-16 11:19:31 -0500

    Code as of video Ch. 1.14

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 struct ContentView: View {
   @State private var messageString = ""
   @State private var imageName = ""
-  @State private var imageNumber = 0
-  @State private var messageNumber = 0
   
   var body: some View {
     VStack {
@@ -45,18 +43,10 @@ struct ContentView: View {
           "Fabulous? That's You!",
           "You Make Me Smile!",
           "When the Genius Bar Needs Help, They Call You!"
-          ]
+        ]
         
-        messageString = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-          messageNumber = 0
-        }
-        imageName = "image\(imageNumber)"
-        imageNumber += 1
-        if imageNumber > 9 {
-          imageNumber = 0
-        }
+        messageString = messages[Int.random(in: 0...messages.count-1)]
+        imageName = "image\(Int.random(in: 0...9))"
       }
       .buttonStyle(.borderedProminent)
       
