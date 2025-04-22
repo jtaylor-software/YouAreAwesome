3935b22ea1dd30a8370855aa01eb62afb4fef60b
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-22 10:42:54 -0500

    UI Changes

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,25 +10,39 @@ import SwiftUI
 struct ContentView: View {
   @State private var message = "I Am A Programmer!"
   
-    var body: some View {
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-          Text(message)
-            .font(.largeTitle)
-            .fontWeight(.heavy)
-            .foregroundStyle(.red)
-          
-          Button("Click Me!") {
-            message = "Awesome!"
-          }
+  var body: some View {
+    VStack {
+      
+      Spacer()
+      Image(systemName: "swift")
+        .resizable()
+        .scaledToFit()
+        .foregroundStyle(.orange)
+        .frame(width: 200, height: 200)
+      Text(message)
+        .font(.largeTitle)
+        .fontWeight(.ultraLight)
+      
+      Spacer()
+      
+      HStack {
+        Button("Awesome") {
+          message = "Awesome!"
         }
-        .padding()
+        
+        Button("Great") {
+          message = "Great!"
+        }
+        
+      }
+      .buttonStyle(.borderedProminent)
+      .font(.title2)
+      .tint(.orange)
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
