ff51364ea4d5437c5cd4ff568328a4239efa5d23
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-23 13:48:43 -0500

    Use an array for messages

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var message = ""
+  @State private var messageNumber = 0
   @State private var imageName = ""
   @State private var imageNumber = 0
   
@@ -27,15 +28,23 @@ struct ContentView: View {
         .font(.largeTitle)
         .fontWeight(.heavy)
         .foregroundStyle(.red)
+        .multilineTextAlignment(.center)
       
       Spacer()
       
       Button("Show Message") {
-        let message1 = "You are Awesome!"
-        let message2 = "You are Great!"
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "Fabulous? That's You!",
+                        "You Make Me Smile!",
+                        "When The Genius Bar Needs Help, They Call You!"]
+        message = messages[messageNumber]
+        messageNumber += 1
         
-      
-        message = (message == message1 ? message2 : message1)
+        if messageNumber == messages.count {
+          messageNumber = 0
+        }
         imageName = "image\(imageNumber)"
         imageNumber += 1
         
