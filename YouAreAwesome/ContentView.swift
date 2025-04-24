06a3f9fea81c81b2a2e0acbe6c056a9aaa7d9eb1
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-24 14:02:16 -0500

    Center the image and play random sound when button is tapped

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
   @State private var message = ""
   @State private var imageName = ""
   @State private var lastMessageNumber = -1
   @State private var lastImageNumber = -1
+  @State private var lastSoundNumber = -1
+  @State private var audioPlayer: AVAudioPlayer!
+  
+  let numberOfImages = 10
+  let numberofSounds = 6
   
   var body: some View {
     VStack {
@@ -23,6 +29,8 @@ struct ContentView: View {
         .minimumScaleFactor(0.5)
         .frame(height: 130)
         .animation(.easeInOut(duration: 0.15), value: message)
+      
+      Spacer()
       
       Image(imageName)
         .resizable()
@@ -50,10 +58,27 @@ struct ContentView: View {
         
         var imageNumber: Int
         repeat{
-          imageNumber = Int.random(in: 0...9)
+          imageNumber = Int.random(in: 0...numberOfImages-1)
         } while imageNumber == lastImageNumber
         imageName = "image\(imageNumber)"
         lastImageNumber = imageNumber
+        
+        var soundNumber: Int
+        repeat {
+          soundNumber = Int.random(in: 0...numberofSounds-1)
+        } while soundNumber == lastSoundNumber
+        lastSoundNumber = soundNumber
+        let soundName = "sound\(soundNumber)"
+        guard let soundFile = NSDataAsset(name: soundName) else {
+          print("😡 Could not read file named \(soundName)")
+          return
+        }
+        do {
+          audioPlayer = try AVAudioPlayer(data: soundFile.data)
+          audioPlayer.play()
+        } catch {
+          print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
       }
       .buttonStyle(.borderedProminent)
       .font(.title2)
